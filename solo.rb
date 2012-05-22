07677644aeb416c507e6583ffa0f70eb198c9896
Bryan Kroger <bryan.kroger@hp.com>
2012-05-22 10:45:58 -0700

    Minor tweak

--- solo.rb
+++ solo.rb
@@ -8,12 +8,13 @@ cache_options({ :path => "%s/.chef/checksums" % ENV['HOME'], :skip_expires => tr
 file_cache_path "%s/.chef/cache/" % ENV['HOME']
 file_backup_path "%s/.chef/cache/" % ENV['HOME']
 
+role_paths = []
 cookbook_paths = []
 
 ["kson-chef","application_name"].each do |appName|
 	role_paths.push( "/var/www/%s/chef/roles" % appName )
 	cookbook_paths.push( "/var/www/%s/chef/cookbooks" % appName )
-]
+end
 coobook_path cookbook_paths
 role_path role_paths
 
