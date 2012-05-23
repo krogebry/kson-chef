815a6d0088b3ed07b750bf4e40d0b85f37e95ce5
Bryan Kroger <bryan.kroger@hp.com>
2012-05-23 13:29:25 -0700

    Adding roles.

--- cookbooks/mysql/definitions/mysql_user.rb
+++ cookbooks/mysql/definitions/mysql_user.rb
@@ -1,18 +1,20 @@
 ##
 # MySQL user setup
 
-execute "Apply MySQL Permissions" do
-	action :nothing
-	command "mysql -u root -p#{node['mysql']['server_root_password']} < /etc/mysql/accessapi-grants.sql"
-end
+define :mysql_user do
+	execute "Apply MySQL Permissions" do
+		action :nothing
+		command "mysql -u root -p#{node['mysql']['server_root_password']} < /etc/mysql/accessapi-grants.sql"
+	end
 
-template "/etc/mysql/%s-grants.sql" do
-	mode "0644"
-	owner "root"
-	group "root"
-	source "grants.sql.erb"
-	notifies :run, resources(:execute => "Apply MySQL Permissions"), :immediately
-	variables({
-	})
+	template "/etc/mysql/%s-grants.sql" do
+		mode "0644"
+		owner "root"
+		group "root"
+		source "grants.sql.erb"
+		notifies :run, resources(:execute => "Apply MySQL Permissions"), :immediately
+		variables({
+		})
+	end
 end
 
