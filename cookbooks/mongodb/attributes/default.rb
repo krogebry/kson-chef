e8aac07c3861bdb4b7929fec1315e9c42246d5e2
Bryan Kroger <bryan.kroger@hp.com>
2012-05-23 14:35:52 -0700

    Updating mongodb recipe.

--- cookbooks/mongodb/attributes/default.rb
+++ cookbooks/mongodb/attributes/default.rb
@@ -2,6 +2,6 @@
 # Default attributes for MongoDB.
 
 default["mongodb"] = {}
-default["mongodb"]["fs_etc_dir"] = "/etc/mongodb"
-default["mongodb"]["fs_log_dir"] = "/var/log/mongodb"
-default["mongodb"]["fs_data_dir"] = "/mnt/data/mongodb"
+default["mongodb"]["fs_etc_root"] = "/etc/mongodb"
+default["mongodb"]["fs_log_root"] = "/var/log/mongodb"
+default["mongodb"]["fs_data_root"] = "/mnt/data/mongodb"
