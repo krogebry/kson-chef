087cd6c40eca65d28447f44716b567e3287aeb78
Bryan Kroger <bryan.kroger@hp.com>
2012-05-23 13:52:53 -0700

    Updating mongodb recipe.

--- cookbooks/mongodb/recipes/server.rb
+++ cookbooks/mongodb/recipes/server.rb
@@ -7,12 +7,18 @@
 # All rights reserved - Do Not Redistribute
 #
 
-template "/etc/mongodb/mongodb.conf" do
+directory node.default["mongodb"]["fs_etc_root"] do
+	owner "root"
+	group "root"
+	action :create
+	recursive true
+end
+
+template "%s/mongodb.conf" % node.default["mongodb"]["fs_etc_root"] do
 	mode "0755"
 	owner "root"
 	group "root"
 	source "mongodb.conf.erb"
 	variables({
-		
 	})
 end
