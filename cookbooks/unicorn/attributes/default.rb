8b0632af55cd7890549043e7b948381292810713
Bryan Kroger <bryan.kroger@hp.com>
2012-05-25 14:28:30 -0700

    Tweaking recipe

--- cookbooks/unicorn/attributes/default.rb
+++ cookbooks/unicorn/attributes/default.rb
@@ -2,7 +2,8 @@
 # Unicorn defaults.
 
 default["unicorn"] = {}
-default["unicorn"]["fs_pid_root"]	= "/var/run/unicorn"
-default["unicorn"]["fs_log_root"] = "/var/log/unicorn"
+default["unicorn"]["fs_app_root"]	= "/var/www/"
 default["unicorn"]["fs_etc_root"] = "/etc/unicorn"
+default["unicorn"]["fs_log_root"] = "/var/log/unicorn"
+default["unicorn"]["fs_pid_root"]	= "/var/run/unicorn"
 
